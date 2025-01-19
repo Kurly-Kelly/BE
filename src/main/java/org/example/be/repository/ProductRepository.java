1ae82a1c37e58a36a36332e5f15a752c9e250be3
yoonhyojin <dbsgywls9855@gmail.com>
2025-01-19 22:09:19 +0900

    검색- 필터기능 구현 중

--- src/main/java/org/example/be/repository/ProductRepository.java
+++ src/main/java/org/example/be/repository/ProductRepository.java
@@ -8,13 +8,14 @@ import org.example.be.domain.SubCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     // 상품 이름에 keyword 를 포함한 상품 검색- 대소문자 구분 안함
     List<Product> findByNameContainingIgnoreCase(String keyword);
     List<Product> findBySubCategory(SubCategory subCategory);
@@ -28,6 +29,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 //    List<Product> findById(Long productId);
 //    @Query("SELECT MAX(p.id) FROM Product p")
 //    Long findMaxProductId();
+
+
+
 
 
 }
